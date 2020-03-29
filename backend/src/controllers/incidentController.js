359a964ee80dd227ab3b4747015584c1ff21b6d8
Christian Lira <christianlira98@gmail.com>
2020-03-28 21:19:07 -0300

    backend da aplicação

---
+++ backend/src/controllers/incidentController.js
@@ -0,0 +1,49 @@
+const connection = require('../database/connection')
+
+module.exports = {
+    async create(request, response) {
+        const {title, description, value} = request.body
+        
+        const ong_id = request.headers.authorization
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id
+        })
+        response.header('X-Total-Count', count['count(*)'])
+        return response.json({id})
+    },
+    async index(request, response) {
+        const {page = 1} = request.query
+        const [count] = await connection('incidents').count()
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select(['incidents.*', 
+                    'ongs.email',
+                    'ongs.whatsapp',
+                    'ongs.city',
+                    'ongs.uf'])
+        return response.json(incidents)
+    },
+    async delete(request, response) {
+        const param_id = request.params.id
+        const ong_id = request.headers.authorization
+        const incident = await connection('incidents')
+            .where('id', param_id)
+            .select('ong_id')
+            .first()
+        
+        if(incident.ong_id !== ong_id) {
+            return response.status(401).json({error: 'Operation not allowed. '})
+        }
+
+        await connection('incidents')
+        .where('id', param_id)
+        .delete()
+
+        return response.status(204).send()
+    }
+}
